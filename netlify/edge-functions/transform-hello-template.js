e079d828467a69c410a5a0243630567603578746
Mark Valles <mvalles36@users.noreply.github.com>
2022-10-13 23:07:20 -0500

    Update transform-hello-template.js

--- netlify/edge-functions/transform-hello-template.js
+++ netlify/edge-functions/transform-hello-template.js
@@ -12,13 +12,13 @@ export default async (request, context) => {
   const page = await response.text();
 
   // Search for the placeholder
-  const regex = /LOCATION_UNKNOWN/i;
+  const regex = /IMAGE_UNKNOWN/i;
 
   // Get the location from the context object
-  const image = `${context.geo.city}, ${context.geo.country.name}`;
+  const image = ``;
 
   // Replace the content with the current location
-  const updatedPage = page.replace(regex, image);
+  const updatedPage = page.replace("https://img1.niftyimages.com/gb/im3d/0etd?" + regex);
 
   // Return the response
   return new Response(updatedPage, response);
